28678f8430b7e03123b6f2c03fcc39bafc3dace1
Kevin <klzheng@buffalo.edu>
2022-11-23 20:21:49 -0500

    Created base map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import esriConfig from "@arcgis/core/config.js"
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App() {
+
+    esriConfig.apiKey = process.env.REACT_APP_ARCGIS_API_KEY
+    const mapDiv = useRef(null);
+
+    useEffect(() => {
+        if (mapDiv.current) {
+            // initialize app
+
+            const map = new Map({
+                basemap: "arcgis-dark-gray" // Basemap layer service
+            })
+
+            const view = new MapView({
+                map: map,
+                center: [-73.9956, 40.6959], // Longitude, latitude
+                zoom: 11, // Zoom level
+                container: "viewDiv" // Div element
+            });
+        }
+    }, []);
+
+    return (
+        <div id="viewDiv" className="mapDiv" ref={mapDiv}>
+        </div>
+    )
 }
-
-export default App;
