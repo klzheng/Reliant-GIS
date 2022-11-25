ab96ea3f791ba0d5e41e379e81f2ff31b0c93e3e
Kevin <klzheng@buffalo.edu>
2022-11-24 23:45:35 -0500

    Adjusted popup title Added household income data by race Added search bar to popup Added popups to all feature layers (tract, county, state)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useRef, useEffect } from "react";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import Search from "@arcgis/core/widgets/Search"
+import Home from "@arcgis/core/widgets/Home"
+import CustomContent from "@arcgis/core/popup/content/CustomContent"
 import esriConfig from "@arcgis/core/config.js"
 import './index.css';
 
@@ -14,28 +17,33 @@ export default function App() {
 
     useEffect(() => {
         if (mapDiv.current) {
-            // initialize app
 
+            // creating tract income feature layer
             const tractIncomeLayer = new FeatureLayer({
                 title: "Household Income",
-                url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Median_Income_by_Race_and_Age_Selp_Emp_Boundaries/FeatureServer/2"
+                url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Median_Income_by_Race_and_Age_Selp_Emp_Boundaries/FeatureServer/2",
+
             })
 
+            // creating county income feature layer
             const countyIncomeLayer = new FeatureLayer({
                 title: "Household Income",
                 url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Median_Income_by_Race_and_Age_Selp_Emp_Boundaries/FeatureServer/1",
             })
 
+            // creating state income feature layer
             const stateIncomeLayer = new FeatureLayer({
                 title: "Household Income",
                 url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Median_Income_by_Race_and_Age_Selp_Emp_Boundaries/FeatureServer/0",
             })
 
+            // creating base map
             const map = new Map({
                 basemap: "arcgis-dark-gray", // Basemap layer service
                 layers: [tractIncomeLayer, countyIncomeLayer, stateIncomeLayer]
             })
 
+            // creating map view
             const view = new MapView({
                 map: map,
                 center: [-73.9956, 40.6959], // Longitude, latitude
@@ -48,44 +56,374 @@ export default function App() {
                         buttonEnabled: false,
                         breakpoint: false
                     }
-                }
+                },
             });
 
-            const popUp = {
-                title: "{County}, {STATE}",
+            // creating new search widget
+            const searchWidget = new Search({
+                view: view,
+            })
+
+            // clears search results whenever a search is completed
+            searchWidget.on("search-complete", (searchResult) => {
+                searchWidget.clear()
+            });
+
+            // creating custom component from search widget
+            const contentWidget = new CustomContent({
+                outFields: ["*"],
+                creator: () => {
+                    return searchWidget;
+                }
+            })
+
+            // popup for tract income feature layer
+            const popUpTract = {
+                title: "{NAME} in {STATE}",
                 alignment: "top-right",
                 content: [
+                    contentWidget,
+                    {
+                        type: "text",
+                        text: "The median household income in this area is estimated to be <b>${B19049_001E}</b>.",
+                    },
+                    {
+                        type: "text",
+                        text: "The total number of households in this area is estimated to be {B19053_001E}.",
+                    },
+                    {
+                        type: "media",
+                        mediaInfos: [
+                            {
+                                title: "Median Householde Income ($)",
+                                caption: "By age",
+                                type: "column-chart",
+                                value: {
+                                    fields: ["B19049_002E", "B19049_003E", "B19049_004E", "B19049_005E",]
+                                },
+                            }
+                        ]
+                    },
                     {
                         type: "fields",
                         fieldInfos: [
                             {
-                                fieldName: "B12001_calc_pctMarriedE",
-                                label: "Married %"
+                                fieldName: "B19013H_001E",
+                                label: "White",
+                                format: {
+                                    digitSeparator: true
+                                }
                             },
                             {
-                                fieldName: "B12001_calc_numMarriedE",
-                                label: "People Married"
+                                fieldName: "B19013B_001E",
+                                label: "Black",
+                                format: {
+                                    digitSeparator: true
+                                }
                             },
                             {
-                                fieldName: "B12001_calc_numNeverE",
-                                label: "People that Never Married"
+                                fieldName: "B19013D_001E",
+                                label: "Asian",
+                                format: {
+                                    digitSeparator: true
+                                }
                             },
                             {
-                                fieldName: "B12001_calc_numDivorcedE",
-                                label: "People Divorced"
-                            }
-                        ]
+                                fieldName: "B19013I_001E",
+                                label: "Hispanic or Latino",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013C_001E",
+                                label: "Native American",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013E_001E",
+                                label: "Hawaiian and Other Pacific Islander",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013G_001E",
+                                label: "2 or More Races",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013F_001E",
+                                label: "Other",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                        ],
                     }
-                ]
+                ],
+                fieldInfos: [
+                    {
+                        fieldName: "B19049_002E",
+                        label: "< 25 years old ",
+                    },
+                    {
+                        fieldName: "B19049_003E",
+                        label: "25 - 44 years old",
+                    },
+                    {
+                        fieldName: "B19049_004E",
+                        label: "45 - 65 years old",
+
+                    },
+                    {
+                        fieldName: "B19049_005E",
+                        label: "> 65 years old",
+                    }
+                ],
             }
 
-            tractIncomeLayer.popupTemplate = popUp
+            // popup for county income feature layer
+            const popUpCounty = {
+                title: "{NAME} in {STATE}",
+                alignment: "top-right",
+                content: [
+                    contentWidget,
+                    {
+                        type: "text",
+                        text: "The median household income in this area is estimated to be <b>${B19049_001E}</b>.",
+                    },
+                    {
+                        type: "text",
+                        text: "The total number of households in this area is estimated to be {B19053_001E}.",
+                    },
+                    {
+                        type: "media",
+                        mediaInfos: [
+                            {
+                                title: "Median Householde Income ($)",
+                                caption: "By age",
+                                type: "column-chart",
+                                value: {
+                                    fields: ["B19049_002E", "B19049_003E", "B19049_004E", "B19049_005E",]
+                                },
+                            }
+                        ]
+                    },
+                    {
+                        type: "fields",
+                        fieldInfos: [
+                            {
+                                fieldName: "B19013H_001E",
+                                label: "White",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013B_001E",
+                                label: "Black",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013D_001E",
+                                label: "Asian",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013I_001E",
+                                label: "Hispanic or Latino",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013C_001E",
+                                label: "Native American",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013E_001E",
+                                label: "Hawaiian and Other Pacific Islander",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013G_001E",
+                                label: "2 or More Races",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013F_001E",
+                                label: "Other",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                        ],
+                    }
+                ],
+                fieldInfos: [
+                    {
+                        fieldName: "B19049_002E",
+                        label: "< 25 years old ",
+                    },
+                    {
+                        fieldName: "B19049_003E",
+                        label: "25 - 44 years old",
+                    },
+                    {
+                        fieldName: "B19049_004E",
+                        label: "45 - 65 years old",
+
+                    },
+                    {
+                        fieldName: "B19049_005E",
+                        label: "> 65 years old",
+                    }
+                ],
+            }
+            
+            // popup for state income feature layer
+            const popUpState = {
+                title: "{NAME} in {STATE}",
+                alignment: "top-right",
+                content: [
+                    contentWidget,
+                    {
+                        type: "text",
+                        text: "The median household income in this area is estimated to be <b>${B19049_001E}</b>.",
+                    },
+                    {
+                        type: "text",
+                        text: "The total number of households in this area is estimated to be {B19053_001E}.",
+                    },
+                    {
+                        type: "media",
+                        mediaInfos: [
+                            {
+                                title: "Median Householde Income ($)",
+                                caption: "By age",
+                                type: "column-chart",
+                                value: {
+                                    fields: ["B19049_002E", "B19049_003E", "B19049_004E", "B19049_005E",]
+                                },
+                            }
+                        ]
+                    },
+                    {
+                        type: "fields",
+                        fieldInfos: [
+                            {
+                                fieldName: "B19013H_001E",
+                                label: "White",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013B_001E",
+                                label: "Black",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013D_001E",
+                                label: "Asian",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013I_001E",
+                                label: "Hispanic or Latino",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013C_001E",
+                                label: "Native American",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013E_001E",
+                                label: "Hawaiian and Other Pacific Islander",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013G_001E",
+                                label: "2 or More Races",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                            {
+                                fieldName: "B19013F_001E",
+                                label: "Other",
+                                format: {
+                                    digitSeparator: true
+                                }
+                            },
+                        ],
+                    }
+                ],
+                fieldInfos: [
+                    {
+                        fieldName: "B19049_002E",
+                        label: "< 25 years old ",
+                    },
+                    {
+                        fieldName: "B19049_003E",
+                        label: "25 - 44 years old",
+                    },
+                    {
+                        fieldName: "B19049_004E",
+                        label: "45 - 65 years old",
+
+                    },
+                    {
+                        fieldName: "B19049_005E",
+                        label: "> 65 years old",
+                    }
+                ],
+            }
+
+            // creating home icon
+            const homeIcon = new Home({
+                view: view
+            })
+
+            // adding home icon to the ui
+            view.ui.add(homeIcon, "top-left");
+
+            // adding popup to each of the feature layers
+            tractIncomeLayer.popupTemplate = popUpTract
+            countyIncomeLayer.popupTemplate = popUpCounty
+            stateIncomeLayer.popupTemplate = popUpState
         }
     }, []);
 
 
     return (
-        <div className="mapDiv" ref={mapDiv}>
-        </div>
+        <div className="mapDiv" ref={mapDiv}></div>
     )
 }
